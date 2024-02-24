adf36b44902b5235aae76914163adf6461fbf733
Emine <80106724+EmineDS@users.noreply.github.com>
2024-02-24 23:14:18 +0300

    data

---
+++ data_manipulation/one-hotEncoding.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing as pr
+veri=pd.read_excel(".\onehotencoding.xlsx")
+print(veri)
+pd.set_option('display.max_rows', None)  # Tüm satırları göster
+pd.set_option('display.max_columns', None)  # Tüm sütunları göster
+#burada eğitim kategorik değişkeni hiyerarşik bir yapı olmasına rağmen
+# diğer kategorik değişkneler cinsiyet ve araç rengi hiyerarşik gruplanmamıştır.
+veri["cinsiyetkod"]=pr.LabelEncoder().fit_transform(veri.Cinsiyet)
+veri["AraçRenkkod"]=pr.LabelEncoder().fit_transform(veri.AraçRenk)
+veri["Eğitimkod"]=pr.LabelEncoder().fit_transform(veri.Eğitim)
+#hepsinin tek seferde kodlanabileceği fonksiyonlar da varmış. araştırılabilir.
+print(veri.sort_values(by=["Eğitimkod"]))#eğitim koduna göre sıraladık
+#diğer kategorik değişkenlerin hiyerarşik olmadığını bilgisayara söylemeliyiz
+onehotdonusum=pd.get_dummies(veri,columns=["Cinsiyet","AraçRenk"], dtype=int)
+print(onehotdonusum)
+#aralarında hiyerarşi tanımlamaması için kategorik değişkenleri parçaladı ve o şekilde numaralandırdı
